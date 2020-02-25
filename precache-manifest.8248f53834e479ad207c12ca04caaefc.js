bf52963d90e08d99236329e07caac794c74f8b54
root <root@DESKTOP-S1QTUB1.localdomain>
2020-02-25 22:25:31 +0900

    Updates

---
+++ precache-manifest.8248f53834e479ad207c12ca04caaefc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42a0e5ff2b7afb96eb0606c4acdb0be",
+    "url": "/https//barosmk.github.io/Nomflix/index.html"
+  },
+  {
+    "revision": "e4661ec34751c2b00a4d",
+    "url": "/https//barosmk.github.io/Nomflix/static/js/2.4460bec2.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/https//barosmk.github.io/Nomflix/static/js/2.4460bec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57cc1c5a863a003059c",
+    "url": "/https//barosmk.github.io/Nomflix/static/js/main.2194f6be.chunk.js"
+  },
+  {
+    "revision": "a925d04cbdc8c15f86f6",
+    "url": "/https//barosmk.github.io/Nomflix/static/js/runtime-main.6907c656.js"
+  }
+]);
